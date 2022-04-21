56add9cf6462a16f6d1fec838e12cc52762484d0
Marketing Strategy <marcelosanchezvasquez14@gmail.com>
2022-04-21 10:44:57 -0500

    1.0.0 react native

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>¡Hola de nuevo !</Text>
       <StatusBar style="auto" />
     </View>
   );
